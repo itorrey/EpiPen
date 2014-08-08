808b42122b739b6827c26e86db8fd9b2429555b0
Torrey Rice <rice@itorrey.com>
2014-08-08 08:53:14 -0600

    Getting local variables to work propertly

--- examples/epi_inject_test/js/custom_func_test.js
+++ examples/epi_inject_test/js/custom_func_test.js
@@ -1,4 +1,11 @@
 function do_custom(txt) {
-	var val = $axure('@entry').$()[0].firstElementChild.value;
-	$axure('@target').$()[0].firstElementChild.value = val;
+	//var val = $axure('@entry').$()[0].firstElementChild.value;
+	//$axure('@target').$()[0].firstElementChild.value = val;
+	//var theJson = document.getElementById("axureEventReceiverDiv").innerHTML;
+	//eval(JSON.parse(theJson).data.epi);
+	console.log(window.$axure)
+}
+
+function con(txt) {
+	console.log(txt);
 }
--- plugins/epi/epi.js
+++ plugins/epi/epi.js
@@ -1,38 +1,61 @@
 (function () {
-    $(window.document).ready(function () {
-        $axure('@EpiInjector').each(function(index, value){
-        	$(index.data).each(function(index, value) {
-                var type = value.type.text;
-                var src = value.src.text;
-                var target = value.target.text;
+    $axure.internal(function($ax){
+        $ax.public.getGlobalVariable = $ax.getGlobalVariable = function(name) {
+            return $ax.globalVariableProvider.getVariableValue(name);
+        };
 
-                createTag(type, src, target);
-        	});
-        });
+        $ax.public.globalVariableProvider = function () {
+            return $ax.globalVariableProvider;
+        };
+
+        $ax.public.document = function() {
+            return $ax.document;
+        };
     });
+
+    $('#axureEventReceiverDiv').bind("DOMSubtreeModified",function(){
+        var request = JSON.parse($(this).text());
+        if(request.data.epi) {
+            eval($axure.getGlobalVariable('epi'));
+        }
+    });
+
+    $axure('@EpiInjector').each(function(index, value){
+    	$(index.data).each(function(index, value) {
+            var type = value.type.text;
+            var src = value.src.text;
+            var target = value.target.text;
+            createTag(type, src, target);
+    	});
+    });
+
 
     function createTag(type, src, target) {
         var tag;
         if(!target) { target = "head"; }
-
         switch(type) {
             case "css":
-                tag = '<link href="'+src+'" rel="stylesheet" type="text/css">';
+                tag = document.createElement("link");
+                tag.href = src;
+                tag.type = "text/css";
+                tag.rel = "stylesheet";
                 break;
 
             case "js":
-                tag = '<script src="'+src+'"></script>';
+                tag = document.createElement("script");
+                tag.src = src;
                 break;
 
             case "html":
-                tag = src;
+                tag = document.createDocumentFragment(src);
                 break;
         }
-
         if(target.substring(0,1) == "@") {
-            $(tag).appendTo($axure(target).$());
+            $axure(target).$()[0].innerHTML = src;
+
         } else {
-            $(tag).appendTo(target);
+            var head = document.getElementsByTagName("head")[0];
+            head.appendChild(tag);
         }
     }
 
