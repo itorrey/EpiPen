1e29c56a31dba6e266b1491bfdc50dc4a510180a
Torrey Rice <rice@itorrey.com>
2014-08-08 21:28:09 -0700

    Reworking some internal epi functions

--- plugins/epi/epi.js
+++ plugins/epi/epi.js
@@ -1,36 +1,20 @@
-(function () {
-    $axure.internal(function($ax){
-        $ax.public.getGlobalVariable = $ax.getGlobalVariable = function(name) {
-            return $ax.globalVariableProvider.getVariableValue(name);
-        };
+epi = {
 
-        $ax.public.globalVariableProvider = function () {
-            return $ax.globalVariableProvider;
-        };
+    currentEpiValue: "",
 
-        $ax.public.document = function() {
-            return $ax.document;
-        };
-    });
+    // Get the data inside the EpiInjector object and create tags from it.
+    inject: function() {
+        $axure('@EpiInjector').each(function(index, value){
+            $(index.data).each(function(index, value) {
+                var type = value.type.text;
+                var src = value.src.text;
+                var target = value.target.text;
+                epi.createTag(type, src, target);
+            });
+        });
+    },
 
-    $('#axureEventReceiverDiv').bind("DOMSubtreeModified",function(){
-        var request = JSON.parse($(this).text());
-        if(request.data.epi) {
-            eval($axure.getGlobalVariable('epi'));
-        }
-    });
-
-    $axure('@EpiInjector').each(function(index, value){
-    	$(index.data).each(function(index, value) {
-            var type = value.type.text;
-            var src = value.src.text;
-            var target = value.target.text;
-            createTag(type, src, target);
-    	});
-    });
-
-
-    function createTag(type, src, target) {
+    createTag: function(type, src, target) {
         var tag;
         if(!target) { target = "head"; }
         switch(type) {
@@ -57,6 +41,27 @@
             var head = document.getElementsByTagName("head")[0];
             head.appendChild(tag);
         }
-    }
+    },
 
+    //Listen for changes to epi variable from Axure.
+    listen: function() {
+        $('#axureEventReceiverDiv').bind("DOMSubtreeModified",function(){
+
+            var request = JSON.parse($(this).text());
+
+            if(request.data.epi && request.data.epi != epi.currentEpiValue) {
+                epi.currentEpiValue = request.data.epi;
+                eval(epi.currentEpiValue);
+            }
+        });
+    },
+
+    init: function() {
+        this.inject();
+        this.listen();
+    }
+};
+
+(function () {
+    epi.init();
 })();
